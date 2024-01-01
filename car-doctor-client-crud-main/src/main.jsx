8190032488bd14cd67991a5d494d618b016abb5f
RF-Rifat <rfrifat6344@gmail.com>
2024-01-01 16:56:20 +0600

    toaset added

--- car-doctor-client-crud-main/src/main.jsx
+++ car-doctor-client-crud-main/src/main.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routes/Routes.jsx';
-import AuthProvider from './providers/AuthProvider';
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import AuthProvider from "./providers/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className='max-w-7xl mx-auto'>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
     <React.StrictMode>
+      <Toaster />
       <AuthProvider>
         <RouterProvider router={router} />
       </AuthProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
+    ,
   </div>
-)
+);
--- car-doctor-client-crud-main/src/pages/Home/Services/Services.jsx
+++ car-doctor-client-crud-main/src/pages/Home/Services/Services.jsx
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import ServiceCard from "./ServiceCard";
 import useAxios from "../../../hooks/useAxios";
 
+
 const Services = () => {
   const [services, setServices] = useState([]);
   const axiosSecure = useAxios();
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axiosSecure.get("/services").then((res) => setServices(res.data));
   }, [axiosSecure]);
+
+  const handleSearchData = (e) => {
+    e.preventDefault();
+    
+  };
 
   return (
     <div className="mt-4">
@@ -21,8 +28,8 @@ const Services = () => {
           believable.{" "}
         </p>
       </div>
-      <div className="flex">
-        <form>
+      <div className="flex flex-col">
+        <form className="w-2/4 py-4" onSubmit={(e) => handleSearchData(e)}>
           <label
             htmlFor="default-search"
             className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -48,6 +55,7 @@ const Services = () => {
               </svg>
             </div>
             <input
+              onChange={(e) => setSearch(e.target.value)}
               type="search"
               id="default-search"
               className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
